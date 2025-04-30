085a44bef336af96df051035aa3434654e23783f
Samy <sam.espinoza@duocuc.cl>
2025-04-30 14:25:14 -0400

    Initial commit

---
+++ src/main/java/com/example/Guia/practica/Inventario/de/libros/GuiaPracticaInventarioDeLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Guia.practica.Inventario.de.libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaPracticaInventarioDeLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiaPracticaInventarioDeLibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Guia/practica/Inventario/de/libros/GuiaPracticaInventarioDeLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Guia.practica.Inventario.de.libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiaPracticaInventarioDeLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
